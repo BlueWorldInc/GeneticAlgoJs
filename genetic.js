6b52d9a3d952c31beb7a341f44e124f3f71f3424
BlueWorldInc <keamybakunin@gmail.com>
2020-04-04 14:36:33 +0200

    [add] added test but not working for the moment

--- genetic.js
+++ genetic.js
@@ -9,9 +9,9 @@ class Individual {
 
     constructor() {
         //var t0 = performance.now();
+        this.fitness;
         this.genome = Array(CHROMOSOME_LENGTH).fill();
         this.createGenome();
-        this.fitness;
         this.calcFitness();
         //var t1 = performance.now();
         //console.log("it took " + (t1 - t0) + " ms");
@@ -49,9 +49,9 @@ class Individual {
 
 class Population {
 
-    constructor() {
-        this.genomePopulation = Array(POPULATION_SIZE).fill();
-        this.createPopulation(POPULATION_SIZE);
+    constructor(populationSize) {
+        this.genomePopulation = Array(populationSize).fill();
+        this.createPopulation(populationSize);
     }
 
     createPopulation(populationSize) {
@@ -63,6 +63,8 @@ class Population {
     getFittest() {
         let fittest = this.genomePopulation[0];
         for (let i = 0; i < this.genomePopulation.length; i++) {
+            // console.log(this.genomePopulation.length);
+            // console.log(this.genomePopulation);
             if (this.genomePopulation[i].getFitness() >= fittest.getFitness()) {
                 fittest = this.genomePopulation[i];
             }    
@@ -80,20 +82,39 @@ class GeneticAlgo {
 
     crossOver(individual1, individual2) {
         for (let i = 0; i < Math.floor(CHROMOSOME_LENGTH / 3); i++) {
-            individual1.modifyGene(CHROMOSOME_LENGTH - 1 - i, individual2.genome[CHROMOSOME_LENGTH - 1 - i]);
+            individual1.modifyGene((CHROMOSOME_LENGTH - 1 - i), individual2.genome[CHROMOSOME_LENGTH - 1 - i]);
         }
         return individual1;
     }
 
     getIndividual(population) {
-        var individual = population.genomePopulation[this.getRandom(POPULATION_SIZE)];
+        let newTournamentPopulation = new Population(TOURNAMENT_SIZE);
         for (let i = 0; i < TOURNAMENT_SIZE; i++) {
-            actualIndividual = population.genomePopulation[this.getRandom(POPULATION_SIZE)];
-            if (actualIndividual.getFitness() > individual.getFitness()) {
-                individual = actualIndividual;
+            let randomIndex = Math.floor(Math.random() * POPULATION_SIZE);
+            newTournamentPopulation.genomePopulation[i] = population.genomePopulation[randomIndex];
+        }
+        return newTournamentPopulation.getFittest();
+    }
+
+    getNewPopulation(population) {
+
+        let newPopulation = new Population(POPULATION_SIZE);
+        for (let i = 0; i < POPULATION_SIZE; i++) {
+            let individual1 = this.getIndividual(population); 
+            if (Math.random() > CROSSOVER_RATE) {
+                let individual2 = this.getIndividual(population);
+                individual1 = this.crossOver(individual1, individual2);
+            }
+            newPopulation.genomePopulation[i] = individual1;
+        }
+
+        for (let i = 0; i < POPULATION_SIZE; i++) {
+            if (Math.random() > MUTATION_RATE) {
+                let individual = population.genomePopulation[i];
+                population.genomePopulation[i] = this.mutation(individual);
             }
         }
-        return individual;
+        return newPopulation;
     }
 
     getRandom(r) {
@@ -110,9 +131,33 @@ class GeneticAlgo {
 
 }
 
-var alpha = new Individual();
-var population = new Population();
-var f = population.getFittest();
-console.log(f);
+var population = new Population(100);
+var algo = new GeneticAlgo();
+var safe = 0;
 
+console.log(algo);
+
+while (safe < 20) {
+    population = algo.getNewPopulation(population);
+    // for (let i = 0; i < POPULATION_SIZE; i++) {
+    //     if (Math.random() > CROSSOVER_RATE) {
+    //         let individual1 = population.genomePopulation[i];
+    //         let individual2 = algo.getIndividual(population);
+    //         // console.log("a");
+    //         // console.log(population.genomePopulation[i]);
+    //         population.genomePopulation[i] = algo.crossOver(individual1, individual2);
+    //         // console.log("b");
+    //         // console.log(population.genomePopulation[i]);
+    //     }
+    // }
+    // for (let i = 0; i < POPULATION_SIZE; i++) {
+    //     if (Math.random() > MUTATION_RATE) {
+    //         let individual1 = population.genomePopulation[i];
+    //         population.genomePopulation[i] = algo.mutation(individual1);
+    //     }
+    // }
+
+    console.log(population.getFittest().fitness);
+    safe++;
+}
 
